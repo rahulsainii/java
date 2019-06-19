31b4f1a2e6123123d71df58a65798eefcfc97630
rahulsainii <51743938+rahulsainii@users.noreply.github.com>
2019-06-19 18:48:32 +0530

    18-june

---
+++ 1 set/firstprog.java
@@ -0,0 +1,12 @@
+class first
+{
+	public static void main(String[] args)
+	{
+	  System.out.println("1 2 3 4 5 6 7");
+	  System.out.println("  1 2 3 4 5  ");
+      System.out.println("    1 2 3    ");
+      System.out.println("      1     ");	  
+	  
+	  
+	  }
+}
---
+++ 1 set/name.java
@@ -0,0 +1,12 @@
+class Myname
+{
+    public static void main(String[] args) 
+	{
+		System.out.println("*******");
+		System.out.println("*RAHUL*");
+		System.out.println("*******");
+		
+ 
+	}
+  
+  }
